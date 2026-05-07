37206507736bb2c8c67c34655faa0f3dca810df5
Christoffer <buyonthenet89@gmail.com>
2026-05-08 00:50:06 +0200

    fixed browser-router, nav and homepage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+
+
+
+    return (
+        <div>
+
+            <h1>Välkommen till Hogwarts galleri! YAY!</h1>
+
+            <br />
+
+            <p>Här kan du titta på en massa olika karaktärer från Harry Potter!</p>
+
+        </div>
+    )
+}
+
+export default Home
